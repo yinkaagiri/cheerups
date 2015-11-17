d5a00ea4153800eccf927ac7e504e63de491c3e2
Fatai Agiri <yinka.fatai@gmail.com>
2015-11-17 16:45:17 -0500

    created controller

---
+++ app/controllers/cheerups_controller.rb
@@ -0,0 +1,4 @@
+class CheerupsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'cheerups/index'
+
   devise_for :users
   root to: "cheerups#index"
 

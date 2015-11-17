308bb8b0b269c4c39ac0467370b0ee729cf702d4
Fatai Agiri <yinka.fatai@gmail.com>
2015-11-17 00:23:51 -0500

    DB migrated and models created

--- db/migrate/20151117045636_create_cheerups.rb
+++ db/migrate/20151117045636_create_cheerups.rb
@@ -1,7 +1,12 @@
 class CreateCheerups < ActiveRecord::Migration
   def change
     create_table :cheerups do |t|
-    
+    t.string     :title
+    t.string     :content
+    t.integer    :user_id
+
+    t.timestamps
+    t.references :user
     end
   end
 end
